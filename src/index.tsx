ed88e98a954e4ea660e8f12205a22a1cf4635d69
a.svirin <a.svirin94@gmail.com>
2023-07-30 17:41:27 +0300

    feat: add all pages

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const MIN_OFFERS = 5;
+const MAX_OFFERS = 10;
+
+const getRandomNumber = (min: number, max: number) => Math.round(Math.random() * (max - min) + min);
+
+const offersCount: number = getRandomNumber(MIN_OFFERS, MAX_OFFERS);
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App
+      offersCount = {offersCount}
+    />
   </React.StrictMode>,
 );
