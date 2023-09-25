60cac77142998ab28c1a01f490788b690e9231c6
a.svirin <a.svirin94@gmail.com>
2023-09-25 15:54:24 +0300

    small fixes

--- project/src/components/cities-list/cities-list.tsx
+++ project/src/components/cities-list/cities-list.tsx
@@ -6,23 +6,27 @@ export function CitiesList(): JSX.Element {
   const choosenCity = useAppSelector((state) => state.cityName);
   const dispatch = useAppDispatch();
 
+  const getCitiesList = () => (
+    CITIES_LIST.map((cityName) => {
+      const className = cityName === choosenCity ? 'locations__item-link tabs__item tabs__item--active' : 'locations__item-link tabs__item';
+
+      return (
+        <li className="locations__item" key={cityName} onClick={() => {
+          dispatch(changeCity(cityName));
+          dispatch(fillOffersList());
+        }}
+        >
+          <a className={className} href="#">
+            <span>{cityName}</span>
+          </a>
+        </li>
+      );
+    })
+  );
+
   return (
     <ul className="locations__list tabs__list">
-      {CITIES_LIST.map((cityName) => {
-        const className = cityName === choosenCity ? 'locations__item-link tabs__item tabs__item--active' : 'locations__item-link tabs__item';
-
-        return (
-          <li className="locations__item" key={cityName} onClick={() => {
-            dispatch(changeCity(cityName));
-            dispatch(fillOffersList());
-          }}
-          >
-            <a className={className} href="#">
-              <span>{cityName}</span>
-            </a>
-          </li>
-        );
-      })}
+      {getCitiesList()}
     </ul>
   );
 }
