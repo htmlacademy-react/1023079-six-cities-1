169fb9ec8e6f6c5ece316289db5b89f9e8b260cb
a.svirin <a.svirin94@gmail.com>
2023-10-29 17:25:18 +0300

    deleted mock data

--- project/src/mocks/reviews.ts
+++ project/src/mocks/reviews.ts
@@ -1,5 +1,3 @@
-import { MOCK_REVIEWS_COUNT } from '../consts';
-
 export type ReviewType = {
   comment: string;
   date: string;
@@ -13,20 +11,3 @@ export type ReviewType = {
   };
 }
 
-const REVIEW: ReviewType = {
-  'comment': 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
-  'date': 'Mon Aug 28 2023 18:30:39 GMT+0300 (Москва, стандартное время)',
-  'rating': 5,
-  'user': {
-    'avatarUrl': 'img/avatar-max.jpg',
-    'id': 1,
-    'isPro': false,
-    'name': 'Oliver Conner'
-  }
-};
-
-export const REVIEWS: ReviewType[] = Array.from({length: MOCK_REVIEWS_COUNT}).map((_, i) => {
-  const newReview = {...REVIEW};
-  newReview.id = i;
-  return newReview;
-});
