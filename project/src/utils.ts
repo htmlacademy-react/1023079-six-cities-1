f339de2b9ce3505eb308e95cb0e572f0446f7c6f
a.svirin <a.svirin94@gmail.com>
2023-11-05 20:18:24 +0300

    fixes with switch

--- project/src/utils.ts
+++ project/src/utils.ts
@@ -15,16 +15,14 @@ export const getOffersInNeighbourhood = (offers: OfferType[], id: number) =>
   offers.filter((offer) => offer.id !== id).slice(0, 3);
 
 export const getRating = (rating: number) => {
-  switch(true) {
-    case rating < 1:
-      return { width: '0%' };
-    case rating < 2:
+  switch(rating) {
+    case 1:
       return { width: '20%' };
-    case rating < 3:
+    case 2:
       return { width: '40%' };
-    case rating < 4:
+    case 3:
       return { width: '60%' };
-    case rating < 5:
+    case 4:
       return { width: '80%' };
     default:
       return { width: '100%' };
