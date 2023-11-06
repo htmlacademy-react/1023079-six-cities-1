3d6f1657b15a4696e1dcd291d90163f2008a121e
a.svirin <a.svirin94@gmail.com>
2023-11-06 15:07:55 +0300

    comeback to else if

--- project/src/utils.ts
+++ project/src/utils.ts
@@ -15,16 +15,17 @@ export const getOffersInNeighbourhood = (offers: OfferType[], id: number) =>
   offers.filter((offer) => offer.id !== id).slice(0, 3);
 
 export const getRating = (rating: number) => {
-  switch(rating) {
-    case 1:
-      return { width: '20%' };
-    case 2:
-      return { width: '40%' };
-    case 3:
-      return { width: '60%' };
-    case 4:
-      return { width: '80%' };
-    default:
-      return { width: '100%' };
+  if (rating < 1) {
+    return { width: '0%' };
+  } else if (rating < 2) {
+    return { width: '20%' };
+  } else if (rating < 3) {
+    return { width: '40%' };
+  } else if (rating < 4) {
+    return { width: '60%' };
+  } else if (rating < 5) {
+    return {width: '80%'};
+  } else {
+    return {width: '100%'};
   }
 };
